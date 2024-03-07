fae17f016002161ea789474dab97b65437c94ddf
Anand-Prasad-14 <adrock4321anandgp@gmail.com>
2024-03-07 23:21:33 +0530

    created crimes_list model

---
+++ lib/crime_report/models/crime_list.dart
@@ -0,0 +1,13 @@
+List<String> crimes = [
+  'Theft',
+  'Assault',
+  'Robbery',
+  'Sexual Assault',
+  'Vandalism',
+  'Battery',
+  'Domestic Abuse',
+  'Motor Vehicle Theft',
+  'Fraud',
+  'Corporate Violence',
+  'Illegal Drug Usage'
+];
