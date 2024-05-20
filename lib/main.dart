210c592150454c9825189672d5cab3051cd3d8e2
Anand-Prasad-14 <adrock4321anandgp@gmail.com>
2024-05-20 21:05:29 +0530

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:secure_alert/account/components/webview.dart';
 import 'package:secure_alert/account/views/account_screen.dart';
 import 'package:secure_alert/account/views/edit_profile_view.dart';
+import 'package:secure_alert/account/views/edit_soscontent_view.dart';
 import 'package:secure_alert/account/views/help_center_view.dart';
+import 'package:secure_alert/account/views/manage_view.dart';
 import 'package:secure_alert/authenticate/views/login_screen.dart';
 import 'package:secure_alert/authenticate/views/register_screen.dart';
 import 'package:secure_alert/authenticate/views/splash_scrreen.dart';
@@ -11,27 +15,43 @@ import 'package:secure_alert/crime_report/views/crime_report_view.dart';
 import 'package:secure_alert/home.dart';
 import 'package:secure_alert/service/global.dart';
 import 'package:secure_alert/service/logger.dart';
+import 'package:secure_alert/social_feed/views/my_post_view.dart';
+import 'package:secure_alert/social_feed/views/post_feed_view.dart';
+import 'package:dcdg/dcdg.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyC9l94A3DGGGiospTeV0yzChpOq31PvU4g',
+          appId: '1:468402823127:android:f796c18d48ec78cbfd3592',
+          messagingSenderId: '468402823127',
+          projectId: 'crime-alert-e1c3c',
+          storageBucket: 'crime-alert-e1c3c.appspot.com'));
 
   //awesome notifications
-  AwesomeNotifications().initialize(null, [
+  await AwesomeNotifications().initialize(null, [
     NotificationChannel(
         channelKey: 'basic_channel',
         channelName: 'Basic notifications',
         channelDescription: 'Notification channel for basic tests')
   ]);
+
+  
+
+  bool isAllowedToSendNotification = await AwesomeNotifications().isNotificationAllowed();
+  if (!isAllowedToSendNotification) {
+    AwesomeNotifications().requestPermissionToSendNotifications();
+  }
   runApp(const MyApp());
 }
 
-_init() async {
-  var log = logger();
-  log.i('App init');
-  var global = Global();
-  await global.init();
-  return Future.value(null);
-}
+  Future<void> _init() async {
+    var log = logger();
+    log.i('App init');
+    var global = Global();
+    await global.init();
+  }
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -41,7 +61,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   asyncInit() async {
     await _init();
   }
@@ -51,11 +70,13 @@ class _MyAppState extends State<MyApp> {
     super.initState();
     asyncInit();
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Defender3601',
+      title: 'India Alert',
       theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (context) => const SplashScreen(),
@@ -64,9 +85,15 @@ class _MyAppState extends State<MyApp> {
         '/register': (context) => const RegisterScreen(),
         '/account': (context) => const AccountScreen(),
         '/crimeAlert': (context) => const CrimeAlertView(),
-        '/crimeReport':(context) => const CrimeReportScreen(),
+        '/crimeReport': (context) => const CrimeReportScreen(),
         '/helpCenter': (context) => const HelpCenterView(),
-        '/editProfile': (context) => const EditProfile()
+        '/editProfile': (context) => const EditProfile(),
+        '/postFeed':(context) => const PostFeedScreen(),
+        '/postList':(context) => const PostFeedScreen(),
+        '/manageContact':(context) => const ManageEmergencyContact(),
+        '/editSOS':(context) => const EditSOSContent(),
+        '/myPost':(context) => const PostScreen(),
+        '/guide':(context) => const GuideWebView()
       },
     );
   }
--- lib/service/firebase.dart
+++ lib/service/firebase.dart
@@ -4,6 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:secure_alert/service/global.dart';
+import 'package:secure_alert/social_feed/models/comment_model.dart';
+import 'package:secure_alert/social_feed/models/post_model.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -13,7 +15,9 @@ final databaseReference = FirebaseDatabase.instance.ref();
 
 Future signIn(String email, String password) async {
   try {
-    User? user = (await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password)).user;
+    User? user = (await FirebaseAuth.instance
+            .signInWithEmailAndPassword(email: email, password: password))
+        .user;
 
     SharedPreferences preferences = await SharedPreferences.getInstance();
     preferences.setString("userID", user!.uid);
@@ -27,13 +31,16 @@ Future signIn(String email, String password) async {
 
 Future createAccount(String email, String password, String iNo) async {
   try {
-    User? user = (await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password)).user;
+    User? user = (await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password))
+        .user;
     return user!.uid;
   } on FirebaseAuthException catch (e) {
     if (e.code == 'weak-password') {
       Fluttertoast.showToast(msg: "The password provided is too weak.");
     } else if (e.code == 'email already exists') {
-      Fluttertoast.showToast(msg: "The account is already exists for that email.");
+      Fluttertoast.showToast(
+          msg: "The account is already exists for that email.");
     }
     return false;
   } catch (e) {
@@ -51,15 +58,14 @@ Future signOut() async {
   await Global.instance.logout();
 }
 
-
-Future checkUserExists(String idNo) async{
+Future checkUserExists(String idNo) async {
   bool isDuplicate = false;
   final snapshot = await databaseReference.child('users').get();
   if (snapshot.exists) {
     Map data = await json.decode(json.encode(snapshot.value));
 
     for (var element in data.values) {
-      if(element["iNo"] == idNo){
+      if (element["iNo"] == idNo) {
         isDuplicate = true;
       }
     }
@@ -91,8 +97,8 @@ Future getPostData(String postID) async {
 }
 
 //SOS methods
-Future getSOSData(String userID) async {
-  final snapshot = await databaseReference.child('sos/$userID').get();
+Future getSOSData(String userId) async {
+  final snapshot = await databaseReference.child('sos/$userId').get();
   if (snapshot.exists) {
     return snapshot.value;
   } else {
@@ -103,24 +109,127 @@ Future getSOSData(String userID) async {
 Future getRecipientContact(String userId) async {
   List<String> recipientList = [];
   final contactRef = FirebaseDatabase.instance.ref().child('contacts/$userId');
-  contactRef.onValue.listen((event) async {
+  await contactRef.onValue.listen((event) async {
     for (final child in event.snapshot.children) {
       final contactID = await json.decode(json.encode(child.key));
       Map data = await json.decode(json.encode(child.value));
 
-      recipientList.add(data["contactNo"]);
+      recipientList.add(data["phone"]);
     }
   });
   return recipientList;
 }
 
+Future<List<Post>> getPostList() async {
+  List<Post> postList = [];
+  final postRef = FirebaseDatabase.instance.ref().child('post');
+  await postRef.onValue.listen((event) async {
+    //fetch every post in database
+    for (final child in event.snapshot.children) {
+      List<String> postMedia = [];
+      List<Comment> comments = [];
+
+      //assign postID and post data
+      final postID = await json.decode(json.encode(child.key));
+      Map data = await json.decode(json.encode(child.value));
+
+      //add media files to media list
+      if (data['media'] != null) {
+        for (var i = 0; i < data['media'].length; i++) {
+          postMedia.add(data['media'][i]["file"]);
+        }
+      }
+
+      //add comments to comment list
+      if (data['comments'] != null) {
+        var commentData = data['comments'];
+        commentData.keys.forEach((key) {
+          var commentID = key;
+          var data = commentData[commentID];
+          comments.add(Comment(commentID, data['dateCreated'], data['userID'],
+              data['comment']));
+        });
+      }
+
+      postList.add(Post(
+          postId: postID,
+          userId: data['userID'],
+          firstName: data['userName'],
+          location: data['location'],
+          dateCreated: DateTime.parse(data['dateCreated']),
+          avatar: data['avatar'],
+          content: data['content'],
+          priority: data['priority'],
+          title: data['title'],
+          media: postMedia,
+          comments: comments));
+    }
+  }, onError: (error) {
+    print('Error getting post List');
+  });
+  return postList;
+}
+
+Future<List<Post>> getMyPostList() async {
+  List<Post> postList = [];
+  final postRef = FirebaseDatabase.instance.ref().child('post');
+  await postRef.onValue.listen((event) async {
+    for (final child in event.snapshot.children) {
+      List<String> postMedia = [];
+      List<Comment> comments = [];
+
+      //assign postID and post data to variable
+      final postID = await json.decode(json.encode(child.key));
+      Map data = await json.decode(json.encode(child.value));
+
+      //condition to check if post created by current user
+      if (Global.instance.user!.uId == data['userID']) {
+        //add media files to medialist
+        if (data['media'] != null) {
+          for (var i = 0; i < data['media'].length; i++) {
+            if (data['media'][i] != null) {
+              postMedia.add(data['media'][i]["file"]);
+            }
+          }
+        }
+
+        //add comments to commentlist
+        if (data['comments'] != null) {
+          var commentData = data['comments'];
+          commentData.keys.forEach((key) {
+            var commentID = key;
+            var data = commentData[commentID];
+            comments.add(Comment(commentID, data['dateCreated'], data['userID'],
+                data['comment']));
+          });
+        }
+
+        //add the post to the postlist
+        postList.add(Post(
+            postId: postID,
+            userId: data['userID'],
+            firstName: data['userName'],
+            location: data['location'],
+            dateCreated: DateTime.parse(data['dateCreated']),
+            avatar: data['avatar'],
+            content: data['content'],
+            priority: data['priority'],
+            title: data['title'],
+            media: postMedia,
+            comments: comments));
+      }
+    }
+  }, onError: (error) {
+    print('Error getting post List');
+  });
+  return postList;
+}
+
 Future editAvatarPostList() async {
   final postRef = FirebaseDatabase.instance.ref().child('post');
   await postRef.onValue.listen((event) async {
-    
-    for (var child in event.snapshot.children) {
-      
-      final postID = await jsonDecode(jsonEncode(child.key));
+    for (final child in event.snapshot.children) {
+      final postID = await json.decode(json.encode(child.key));
       Map data = await json.decode(json.encode(child.value));
 
       //If post is created by currentuser
@@ -131,8 +240,7 @@ Future editAvatarPostList() async {
         });
       }
     }
-  },
-  onError: (error){
+  }, onError: (error) {
     print('Error getting post List');
   });
 }
